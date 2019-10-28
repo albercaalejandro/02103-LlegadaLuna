403697ccd58c75e00b92a6c101cdf75695fd5071
Xtek <Xtek@Pc-Nasa>
2019-10-28 17:42:33 +0100

    :metal: Commit resubido

--- src/llegadaluna/Main.java
+++ src/llegadaluna/Main.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2019 Alejandro Alberca Gallardo <alejandro.alberca.alum@iescamp.es>.
+ * Copyright 2019 Alejandro Alberca - alejandro.alberca.alum@iescamp.es.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
